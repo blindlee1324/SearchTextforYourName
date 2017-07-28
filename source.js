78fc2b515cb1fb34d529ef20dbaf5f3edf4fe130
blindlee1324 <aldltekd@naver.com>
2017-07-28 23:05:26 +0900

    for comments

--- source.js
+++ source.js
@@ -1,17 +1,26 @@
+// Blindlee1324 and Brown are mixed up in this text
+// Only Blindlee1324 is what I want to find.
 var text = "Hi, Blindlee1324\
 oh, Brown is here too!\
-Blindlee1324, you bring him here? that Brilliant!\
+Blindlee1324, you bring him here? that's Brilliant!\
 I'm happy to see you all Blindlee1324 and Brown";
+// Here's my name
 var myName = "Blindlee1324";
+// a storage for myName(s)
 var Hits = [];
-var isNameFound = 0; // How many names on hits
+// How many names on Hits?
+var isNameFound = 0; 
 
 for (var i = 0; i <= text.length; i++){
     if (text[i] === myName[0]) {
+    	// temporary variable for storing names on text 
+    	// and comparing that with myName.. Blindlee1324
         var tempName = ""
         for(var j = i; j < (myName.length + i); j++) {
+        	// store string on tempName
             tempName = tempName + text[j];
         };
+        // compare temp and actual myName
         if (tempName === myName) {
             Hits.push(tempName);
             isNameFound++;
@@ -19,6 +28,7 @@ for (var i = 0; i <= text.length; i++){
     };
 };
 
+// is myName on Hits?
 if (isNameFound === 0) {
     console.log("Sorry, " + myName + " was never found!");
 }
